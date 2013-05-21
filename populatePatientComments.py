71dd1a1540e267604e64b702e0eff74667ce5ee3
Andrey Fedorov <fedorov@bwh.harvard.edu>
2013-05-21 10:24:22 -0400

    BUG: fixing errors preventing parsing PatientComments
    * '.' is not allowed in Session name
    * added ';' in the end of PatientComments

--- populatePatientComments.py
+++ populatePatientComments.py
@@ -1,4 +1,4 @@
-import dicom, os, shutil, sys
+import dicom, os, shutil, sys, string
 from dicom.tag import Tag
 
 # input: directory with the DICOM files
@@ -8,17 +8,19 @@ project = sys.argv[2]
 subject = sys.argv[3]
 outputDir = sys.argv[4]
 
-for f in os.listdir(inputDir):
-  print 'Processing ', f
-  inputFile = inputDir+'/'+f
-  dcm = dicom.read_file(inputFile)  
-  outputFile = outputDir+'/'+f
+for r,d,files in os.walk(inputDir):
+  for f in files:
+    print 'Processing ', f
+    inputFile = r+'/'+f
+    dcm = dicom.read_file(inputFile)
+    outputFile = outputDir+'/'+f
 
-  pcTag = Tag(0x10,0x4000)
-  dcm.add_new(pcTag,'LT',"Project: "+project+"; Subject: "+subject+"; Session: "+subject+"_"+dcm.StudyDate+"_"+dcm.StudyTime)
-  
-  print dcm.PatientComments
+    pcTag = Tag(0x10,0x4000)
+    time = string.split(dcm.StudyTime,'.')[0]
+    dcm.add_new(pcTag,'LT',"Project: "+project+"; Subject: "+subject+"; Session: "+subject+"_"+dcm.StudyDate+"_"+time+';')
 
-  f = open(outputFile,'wb')
-  dicom.write_file(outputFile,dcm)
-  # shutil.copyfile(inputFile, outputFile)
+    print dcm.PatientComments
+
+    f = open(outputFile,'wb')
+    dicom.write_file(outputFile,dcm)
+    # shutil.copyfile(inputFile, outputFile)
